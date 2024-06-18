c889b6f51caf350b3a7938164be86c4a7ef089bd
Nicolas Mazzon <nicolasmazzon549@gmail.com>
2024-06-18 12:06:18 +0200

    Print only from main thread.

--- src/main.cc
+++ src/main.cc
@@ -1,13 +1,13 @@
+#include <atomic>
 #include <cassert>
+#include <chrono>
 #include <cstring>
 #include <string>
-#include <chrono>
 #include <thread>
-#include <atomic>
 
 #include "common.hpp"
-#include "metal.hpp"
 #include "hash.hpp"
+#include "metal.hpp"
 
 using namespace std::chrono;
 
@@ -83,39 +83,44 @@ const char* PBSTR = "|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 const u64 PBWIDTH = 60;
 
 void print_progress(double rate, double percentage) {
-    int val = (int) (percentage * 100);
-    int lpad = (int) (percentage * PBWIDTH);
+    int val = (int)(percentage * 100);
+    int lpad = (int)(percentage * PBWIDTH);
     int rpad = PBWIDTH - lpad;
     printf("\r  %.1f KH/s %3d%% [%.*s%*s]", rate, val, lpad, PBSTR, rpad, "");
     fflush(stdout);
 }
 
 struct GlobalContext {
+    u8* mac_ap;
+    u8* mac_sta;
+    u32* target_hash;
+    std::string_view pattern;
+
+    u64 thread_count;
+    u64 hashes_to_check;
+
     std::atomic<bool> found_match;
     std::atomic<u64> total_hash_count;
 };
 
 struct ThreadContext {
-    u8 *mac_ap;
-    u8 *mac_sta;
-    u32 *target_hash;
-    std::string_view pattern;
-
     u64 idx;
-    u64 thread_count;
-    u64 hashes_to_check;
     std::thread thread;
 };
 
 const u64 PRINT_INTERVAL = 500000;
 
-void worker(GlobalContext *gctx, ThreadContext *tctx) {
+void worker(GlobalContext* gctx, ThreadContext* tctx) {
     u32 hash[5];
     u64 hash_count = 0;
-    auto start = high_resolution_clock::now();
+    std::chrono::time_point<std::chrono::high_resolution_clock> start;
 
-    hash::generate_permutations(tctx->pattern, tctx->idx, tctx->thread_count, [&](const u8 test_case[64]) {
-        hash::pmkid(test_case, tctx->mac_ap, tctx->mac_sta, hash);
+    // Only the main thread has to record time for showing the progress.
+    if (tctx->idx == 0)
+         start = high_resolution_clock::now();
+
+    hash::generate_permutations(gctx->pattern, tctx->idx, gctx->thread_count, [&](const u8 tc[64]) {
+        hash::pmkid(tc, gctx->mac_ap, gctx->mac_sta, hash);
         hash_count++;
 
         if (hash_count == PRINT_INTERVAL) {
@@ -126,24 +131,30 @@ void worker(GlobalContext *gctx, ThreadContext *tctx) {
             u64 total_hash_count = gctx->total_hash_count.load(std::memory_order_acquire);
             gctx->total_hash_count.store(total_hash_count + hash_count, std::memory_order_release);
 
-            auto now = high_resolution_clock::now();
-            auto duration = duration_cast<milliseconds>(now - start);
-            double hps = ((double)hash_count / (double)duration.count()) * 1000;
-            double progress = (double)total_hash_count / (double)tctx->hashes_to_check;
-            print_progress(hps / 1024.0, progress);
+            // Only the main thread updates the progress.
+            if (tctx->idx == 0) {
+                auto now = high_resolution_clock::now();
+                auto duration = duration_cast<milliseconds>(now - start);
+                double hps = ((double)hash_count / (double)duration.count()) * 1000;
+                double progress = (double)total_hash_count / (double)gctx->hashes_to_check;
 
-            start = now;
+                // It isn't entirely correct to just multiply the local hash count by the #thread.
+                print_progress(hps * (double)gctx->thread_count / 1024.0, progress);
+
+                start = now;
+            }
+
             hash_count = 0;
         }
 
         for (u64 jdx = 0; jdx < 5; jdx++)
-            if (hash[jdx] != tctx->target_hash[jdx])
+            if (hash[jdx] != gctx->target_hash[jdx])
                 // Keep looking for matching hashes.
                 return true;
 
         std::string out = hash::bytes_to_digest(reinterpret_cast<u8*>(hash), 20);
         printf("\nfound matching hash: %s\n", out.c_str());
-        printf("passphrase is: %s\n", test_case);
+        printf("passphrase is: %s\n", tc);
         gctx->found_match.store(true);
         return false;
     });
@@ -161,7 +172,7 @@ int main(int argc, const char* argv[]) {
     if (argc < 2)
         error("failed to provide pattern, usage: ./metaling {d|l|u|a|?}*\n");
 
-    const char *pattern = argv[1];
+    const char* pattern = argv[1];
 
     // Example packet.
     u8 mac_ap[6];
@@ -181,22 +192,22 @@ int main(int argc, const char* argv[]) {
     printf("detected %lld threads\n", thread_count);
 
     ThreadContext threads[thread_count];
-    GlobalContext gctx = GlobalContext {
+    GlobalContext gctx = GlobalContext{
+        .mac_ap = mac_ap,
+        .mac_sta = mac_sta,
+        .target_hash = target_hash,
+        .pattern = pattern,
+
+        .thread_count = thread_count,
+        .hashes_to_check = hashes_to_check,
+
         .found_match = false,
         .total_hash_count = 0,
     };
 
     for (u64 idx = 0; idx < thread_count; idx++) {
         ThreadContext* tctx = &threads[idx];
-
-        tctx->mac_ap = mac_ap;
-        tctx->mac_sta = mac_sta;
-        tctx->target_hash = target_hash;
-        tctx->pattern = pattern;
-
         tctx->idx = idx;
-        tctx->thread_count = thread_count;
-        tctx->hashes_to_check = hashes_to_check;
         tctx->thread = std::thread(worker, &gctx, tctx);
     }
 
